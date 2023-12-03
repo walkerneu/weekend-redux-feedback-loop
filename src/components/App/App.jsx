aabc7b601bb290d9e1fcdae59c192b8aec822ce4
Walker Neudorff <walkerneudorff@gmail.com>
2023-12-02 18:54:36 -0600

    Imported material ui, added Header component with mui styling, home and admin link buttons

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,16 +11,14 @@ import CommentsInput from '../CommentsInput/CommentsInput';
 import SubmitPage from '../SubmitPage/SubmitPage';
 import SuccessPage from '../SuccessPage/SuccessPage';
 import AdminPage from '../AdminPage/AdminPage';
+import Header from '../Header/Header';
 
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
       <Router>
+        <Header />
         <Route exact path='/'>
           <FeelingsInput />
         </Route>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { useHistory } from 'react-router-dom';
+
+function Header(){
+    const history = useHistory();
+
+    const adminLink = () => {
+        history.push('/admin');
+    }
+    const homeLink = () => {
+        history.push('/');
+    }
+    return  (
+    <Box sx={{ flexGrow: 1 }}
+        className="App-header">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            FEEDBACK
+          </Typography>
+          <Button 
+            color="inherit"
+            onClick={adminLink}>
+                Admin
+          </Button>
+          <Button 
+            color="inherit"
+            onClick={homeLink}>
+                Home
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    )
+}
+
+export default Header;
+
+{/* <header className='App-header'>
+        <h1 className='App-title'>Feedback!</h1> */}
